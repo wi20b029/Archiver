627291d9744f4f27b377b81b26ef7e646558e834
Katharina Schuster <wi20b029@technikum-wien.at>
2021-06-27 15:13:51 +0200

    first runnable version to copy file with timestamp

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import java.io.IOException;
+import java.nio.file.*;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
 public class Main {
-    public static void main (String[] args) {
-        System.out.println("Hello world");
+    public static void main(String[] args) throws IOException {
+        if (args.length != 1) {
+            System.out.println("Insert exactly one filepath.");
+            return;
+        }
+        Path source = Paths.get(args[0]);
+        if (!Files.exists(source)) {
+            System.out.println("File does not exist.");
+            return;
+        }
+
+        String src = source.toString();
+        String[] source_arr = src.split("\\.");
+        if (source_arr.length != 2) {
+            System.out.println("Filepath has no or too many dots. " + src);
+            return;
+        }
+
+        Date date = new Date();
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd HHmmss");
+        String strDate = formatter.format(date);
+
+        Path target = Paths.get(source_arr[0] + strDate + "." + source_arr[1]);
+
+        Files.copy(source, target);
     }
 }
